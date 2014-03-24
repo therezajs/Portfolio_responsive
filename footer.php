db8c15977c817d268b6a988e93db35e1358de63c
Theres Scherrer <theresscherrer@gmail.com>
2014-03-24 10:18:09 -0700

    First commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+  <div id="footer">
+    <!-- <hr> -->
+    <footer>
+      <br>
+      <p id="footer">Made with love by <a href="about.php">Thereza</a>, 2014 | <a href="http://www.linkedin.com/in/thereza">LinkedIn</a> | <a href="https://twitter.com/therezaJS">Twitter</a> | <a href="https://github.com/therezajs">Github</a></p>
+
+    </footer><br>
+  </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="">
+  <meta name="author" content="Thereza Scherrer">
+  <title>Portfolio Thereza Scherrer</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css">
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+  <script src="dist/js/bootstrap.js" ></script>
+</head>
+<body>
+  <nav class="navbar navbar-inverse navbar-fixed-top" id="navbar" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand my_name" href="index.php">THEREZA SCHERRER</a>
+      </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#about">About me/</a></li>
+          <li><a href="#contact">Contact/</a></li>
+          <li><a href="#skills">Skills/</a></li>
+          <li><a href="#current_projects">Latest work/</a></li>
+          <li><a href="#mini_projects">Mini projects/</a></li>
+
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div>
+  </nav>
+
+
+
+
+
