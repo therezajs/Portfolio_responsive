0c91407af86bc827fe64d8ec956835a408bffa62
Theres Scherrer <theresscherrer@gmail.com>
2014-03-25 09:58:24 -0700

    New resume summary

--- index.php
+++ index.php
@@ -3,95 +3,9 @@
 <head>
   <meta charset="UTF-8" />
   <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css" />
+  <link rel="stylesheet" type="text/css" href="style.css">
   <meta name="viewport" content="width=device-width, initial-scale=0.6" />
-  <style>
-    body {
-      font-family:
-        'HelveticaNeue-Light',
-        'Helvetica Neue Light',
-        'Helvetica Neue',
-        Helvetica,
-        Arial;
-      color: #332b22;
-    }
-    .navbar {
-      background-color: #332b22;
-      color: #9acee5;
-    }
-    .navbar, .navbar-brand {
-      font-size: 24px;
-    }
-    #main {
-      background-color: #d6ebf1;
-      margin-top: 50px;
-      padding: 48px 0px 64px;
-      text-align: center;
-      font-size: 48px;
-    }
-    #skills {
-      background-color: #9acee5;
-      padding: 12px 0px 16px;
-    }
-    #skills .row {
-      margin: 0px 8px;
-    }
-    #skills .col-md-2 {
-      color: #06538e;
-      font-size: 28px;
-      text-align: center;
-      padding: 12px;
-    }
-    #portfolio .row {
-      margin: 48px 0px 72px;
-    }
-    #portfolio img {
-      box-shadow: 4px 4px 16px #808080;
-    }
-    #resume {
-      background-color: #d6ebf1;
-      padding: 24px 0px 36px;
-      text-align: center;
-    }
-    #resume .row {
-      margin: 0px 12px;
-    }
-    #resume a.btn {
-      width: 100%;
-      margin: 12px 0px 16px;
-      font-size: 150%;
-    }
-    #resume p {
-      font-size: 21px;
-      text-align: left;
-    }
-    #resume .summary {
-      margin-bottom: 24px;
-    }
-    #resume img {
-      margin-bottom: 36px;
-      width: 100%;
-      max-width: 180px;
-    }
-    footer {
-      background-color: #9acee5;
-      padding: 12px 0px 16px;
-      font-size: 16px;
-      text-align: center;
-    }
-    /*
 
-#332b22 24.5 % Black Magic (Brown)
-#b2a390 22.3 % Eagle (Green)
-#7f8384 11.4 % Grey (Grey)
-#d6ebf1 9.7 % Pattens Blue (Blue)
-#9acee5 9.0 % Cornflower (Blue)
-#836833 8.3 % McKenzie (Brown)
-#4e97c7 6.2 % Picton Blue (Blue)
-#594a29 4.5 % Bronze Olive (Brown)
-#06538e 2.2 % Dark Cerulean (Blue)
-#a48230 1.8 % Reef Gold (Green)
-    */
-  </style>
 </head>
 <body>
   <nav class="navbar navbar-fixed-top">
@@ -156,7 +70,17 @@
         </div>
         <div class="col-md-9 col-xs-8">
         <p>
-          Konnichiwa, I am Thereza. Lorem ipsum dolor sit amet. Hire me. Hire me. Hire me. Your business so much needs me. Lorem and ipsum and dolor and sit all agree in this. Even amet agrees.
+          Konnichiwa, I am Thereza.
+        </p>
+        <p>
+          I'm looking for a job as a web developer.
+        </p>
+        <p>
+          I graduated with a major in Japanese studies and moved to San Francisco in October 2013 to attend a programming bootcamp, <a href="http://codingdojo.com/">Coding Dojo</a>.
+        </p>
+        <p>
+          I love to code, solve challenges and create new apps.
+        </p>
         </div>
       </div>
       <div class="row">
