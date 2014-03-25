7f5e90666c7caf388da74e147f8b45ca2fe10c62
Theres Scherrer <theresscherrer@gmail.com>
2014-03-25 01:04:09 -0700

    Added viewport, again

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
-  <div id="footer" class="container">
+  <br>
+  <div id="footer">
     <!-- <hr> -->
     <footer>
       <br>
-      <p id="footer">Made with love by <a href="about.php">Thereza</a>, 2014 | <a href="http://www.linkedin.com/in/thereza">LinkedIn</a> | <a href="https://twitter.com/therezaJS">Twitter</a> | <a href="https://github.com/therezajs">Github</a></p>
-
+      <p id="footer">Made with love by <a href="about.php">Thereza</a>, 2014 | <a href="http://www.linkedin.com/in/thereza" target="_blank">LinkedIn</a> | <a href="https://twitter.com/therezaJS" target="_blank">Twitter</a> | <a href="https://github.com/therezajs" target="_blank">Github</a></p>
     </footer><br>
-
   </div>
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   <script src="dist/js/bootstrap.min.js"></script>
 </body>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <link rel="stylesheet" type="text/css" href="dist/css/bootstrap.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <style>
     body {
       font-family:
