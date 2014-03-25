bac81e681c79088fdbca998ef3f30e073afc46e2
Theres Scherrer <theresscherrer@gmail.com>
2014-03-25 01:26:44 -0700

    Added links to buttons

--- index.php
+++ index.php
@@ -161,32 +161,32 @@
       </div>
       <div class="row">
         <div class="col-md-4 col-sm-6 col-xs-12">
-          <a class="btn btn-success btn-lg">
+          <a class="btn btn-success btn-lg" href="http://github.com/therezajs" target="_blank">
             Github
           </a>
         </div>
         <div class="col-md-4 col-sm-6 col-xs-12">
-          <a class="btn btn-success btn-lg">
+          <a class="btn btn-success btn-lg" href="http://www.linkedin.com/in/thereza" target="_blank">
             LinkedIn
           </a>
         </div>
         <div class="col-md-4 col-sm-6 col-xs-12">
-          <a class="btn btn-success btn-lg">
+          <a class="btn btn-success btn-lg" href="http://twitter.com/therezajs" target="_blank">
             Twitter
           </a>
         </div>
         <div class="col-md-4 col-sm-6 col-xs-12">
-          <a class="btn btn-success btn-lg">
+          <a class="btn btn-success btn-lg" href="https://www.dropbox.com/s/ety8jcibl3829jh/resume-thereza.pdf" target="_blank">
             Resume
           </a>
         </div>
         <div class="col-md-4 col-sm-6 col-xs-12">
-          <a class="btn btn-success btn-lg">
+          <a class="btn btn-success btn-lg" href="http://therezajs.tumblr.com" target="_blank">
             Blog
           </a>
         </div>
         <div class="col-md-4 col-sm-6 col-xs-12">
-          <a class="btn btn-success btn-lg">
+          <a class="btn btn-success btn-lg" href="mailto:therezascherrer@gmail.com">
             Email
           </a>
         </div>
